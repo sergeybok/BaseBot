f891f5f06dced9ce6ea28f37b6d4e1d785d1b45d
Sergey B <sergeybok@gmail.com>
2023-04-07 19:11:06 -0400

    paths

--- basebot/models/__init__.py
+++ basebot/models/__init__.py
@@ -1,3 +1,3 @@
-# from basebot_models import BaseBot, BaseBotWithDb, BaseBotWithLocalDb
-# from the_message import TheMessage, MessageWrapper, MessageContents
-# from web_models import MessageHistoryRequest, MessageHistoryResponse
+from basebot_models import BaseBot, BaseBotWithDb, BaseBotWithLocalDb
+from the_message import TheMessage, MessageWrapper, MessageContents
+from web_models import MessageHistoryRequest, MessageHistoryResponse
