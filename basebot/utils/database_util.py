0956b0a299544fb80cbc198e293635e73971a360
Sergey B <sergeybok@gmail.com>
2023-04-08 20:50:18 -0400

    fix in database_util

--- basebot/utils/database_util.py
+++ basebot/utils/database_util.py
@@ -7,6 +7,7 @@ import json, time
 from ..models.the_message import TheMessage
 
 USER_ID = 'user_id'
+TO_USER_ID = 'user_id'
 TS = 'timestamp'
 MESSAGE_ID = 'message_id'
 
@@ -23,11 +24,16 @@ class MongoUtil:
         self.mongo.db[name].update_one({ MESSAGE_ID: message.message_id }, { SET: message.dict()}, upsert=True)
 
     def get_chat_messages(self, name:str, user_id:str, limit:int=5, before_ts=None) -> List[TheMessage]:
-        criteria = { USER_ID: { CONTAINS: [name, user_id] }}
+        from_criteria = { USER_ID: user_id }
+        to_criteria = { TO_USER_ID: user_id }
         if before_ts is not None:
-            criteria[TS] = { LESS_THAN: before_ts }
+            from_criteria[TS] = { LESS_THAN: before_ts }
+            to_criteria[TS] = { LESS_THAN: before_ts }
         messages = []
-        for msg_dict in self.mongo.db[name].find(criteria).sort(TS, pymongo.DESCENDING).limit(limit):
+        for msg_dict in self.mongo.db[name].find(from_criteria).sort(TS, pymongo.DESCENDING).limit(limit):
             messages.append(TheMessage.parse_obj(msg_dict))
+        for msg_dict in self.mongo.db[name].find(to_criteria).sort(TS, pymongo.DESCENDING).limit(limit):
+            messages.append(TheMessage.parse_obj(msg_dict))
+        messages = sorted(messages, key=lambda m: m.timestamp, reverse=True)[:limit]
         return messages
 
