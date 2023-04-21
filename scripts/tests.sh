83fe1ae9d8cb1843619c49be6007b0a0da55a039
Alexey O. Medvedev <medvedevthedev@users.noreply.github.com>
2023-04-21 19:39:37 -0400

    Fix tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -10,29 +10,29 @@ import argparse
 from basebot import TheMessage, MessageWrapper, MessageContents
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', type=int, help='localhost:{port}', default=8000)
+parser.add_argument("--port", type=int, help="localhost:{port}", default=8000)
 args = parser.parse_args()
 
-bot_name = input('BotName: ')
-local_uuid = 'test1f3a-97ee-4e7e-8242-7b1d202c0fb5'
-url = f'http://localhost:{args.port}/bots/{bot_name}'
-resp = requests.get(url+'/about').json()
-bot_id = resp['bot_id']
+bot_name = input("Which bot are you trying to test? (NOTE: this should match at least one of the bots passed as arguments to  `BaseBot.start_app(bot)` in your main.py file)\n\nBotName: ")
+local_uuid = "test1f3a-97ee-4e7e-8242-7b1d202c0fb5"
+url = f"http://localhost:{args.port}/bots/{bot_name}"
+resp = requests.get(url+"/about").json()
+bot_id = resp["bot_id"]
 
 def message_init():
     import time 
     import uuid
     return TheMessage(timestamp=time.time(),
                       sender_id=local_uuid, 
-                      contents=MessageContents(text=''),
+                      contents=MessageContents(text=""),
                       message_id=str(uuid.uuid4()),
                       recipient_id=bot_id)
 
 def get_message_to():
-    msg = input('Prompt: ')
+    msg = input("Prompt: ")
     msg_w = message_init()
     msg_w.contents.text = msg
-    resp = requests.post(url + '/respond', json=msg_w.dict())
+    resp = requests.post(url + "/respond", json=msg_w.dict())
     resp = resp.json()
     out_msg = TheMessage.parse_obj(resp)
     print(f"{bot_name}: {out_msg.contents.text}")
