cf1f82a7569ce3d161b5a51af931e6b9b7fcd543
Sergey B <sergeybok@gmail.com>
2023-08-03 13:49:02 -0400

    image_utils added crop

--- basebot/utils/image_utils.py
+++ basebot/utils/image_utils.py
@@ -36,3 +36,42 @@ def b64_string_to_img(im_b64: str) -> Image:
     img2 = Image.open(im_file)   
     return img2
 
+
+def crop_image(vertical:bool, img:Image, percent:int, verbose:bool=True) -> Image:
+    left = 0 
+    upper = 0
+    right = img.width 
+    lower = img.height
+    if vertical:
+        if verbose:
+            print('\tImage:cropping vertical')
+        trim = img.height*(percent / 100)
+        trim_half = round(trim / 2)
+        upper += trim_half
+        lower -= trim_half
+    elif args.horizontal:
+        if verbose:
+            print('Image:cropping horizontal')
+        trim = img.width*(percent / 100)
+        trim_half = round(trim / 2)
+        left += trim_half
+        right -= trim_half
+    img = img.crop((left, upper, right, lower))
+    return img
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--path', type=str, help='Path to image file' )
+    parser.add_argument('-o','--out', type=str, required=False, help='Path to new image file' )
+    parser.add_argument('--vertical', action='store_true')
+    parser.add_argument('--horizontal', action='store_true')
+    parser.add_argument('--pct', type=int, help='Percent to trim using center crop : (0, 100)')
+    args = parser.parse_args()
+    img = Image.open(args.path)
+    
+
+    img.save(args.path)
+
+
